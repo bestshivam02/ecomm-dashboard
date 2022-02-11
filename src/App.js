fdb9dc82ef3882c1b03515a3dd96c722f2ad50b5
Shivam <shivam.kumar@jellyfishtechnologies.com>
2022-02-11 12:37:59 +0530

    React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>E-Comm</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import {Navbar,Nav} from 'react-bootstrap'
+function Header() {
+    return (
+        <div>
+            <h1>YOOOO</h1>
+        </div>
+    )
+}
+
+export default Header
