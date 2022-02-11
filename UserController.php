787abeaebd236029bdf54d2bd6de2dd1df9cf78a
Shivam <shivam.kumar@jellyfishtechnologies.com>
2022-02-11 12:21:54 +0530

    guseg

---
+++ UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    function register(Request $req)
+    {
+        
+        return $req->input();
+    }
+}
