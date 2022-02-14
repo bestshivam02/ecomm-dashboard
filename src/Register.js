5c485457928747a23da3c06364d0607b805112c0
Shivam <shivam.kumar@jellyfishtechnologies.com>
2022-02-14 11:44:49 +0530

    Api integration

--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,38 @@
-import React from 'react';
-function Register() {
+import React,{useState} from 'react';
+
+function Register() 
+{
+    const [name,setName]=useState("");
+    const [password,setPassword]=useState("");
+    const [email,setEmail]=useState("");
+
+async function signUp() 
+{
+    let item = {name,password,email}
+    // console.warn(item)
+    let result = await fetch("http://127.0.0.1:8000/api/register",{
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body:JSON.stringify(item)
+    });
+    result = await result.json();
+    console.warn("result", result)
+}
+
     return(
-        <div>
-            <h1>Register Page</h1>
+        <div className='col-sm-6 offset-sm-3'>
+            <h1>User Sign Up</h1>
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder='Enter Name' className='form-control' />
+            <br />
+            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Enter Password' className='form-control' />
+            <br />
+            <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter Email' className='form-control' />
+            <br />
+            <button onClick={signUp} className='btn btn-primary'>Sign Up</button>
+
         </div>
     )
 }
